454e9aff009b11ff687b86e67e491e387c97c369
Artyom Sog <tomas.tomson@Mail.ru>
2021-05-16 01:09:50 +0300

    feat:  exercise four done

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,37 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    if (array == nil) {return @[];}
+    NSMutableArray<NSNumber*> *numbersArray = [[NSMutableArray alloc] init];
+    NSMutableArray<NSString*> *namesArray = [[NSMutableArray alloc] init];
+    
+    for (NSArray *item in array) {
+        if (![item isKindOfClass:[NSArray class]]) { return @[];}
+        
+        if ([item.firstObject isKindOfClass:[NSNumber class]]) {
+            [numbersArray addObjectsFromArray:item];
+        }
+        if ([item.firstObject isKindOfClass:[NSString class]]) {
+            [namesArray addObjectsFromArray:item];
+        }
+    }
+    [numbersArray setArray:[numbersArray sortedArrayUsingComparator:^NSComparisonResult(NSNumber *first, NSNumber *second) {
+        return [first compare:second];
+    }]];
+    
+    [namesArray setArray:[namesArray sortedArrayUsingComparator:^NSComparisonResult(NSString *first, NSString *second) {
+        return [first compare:second];
+    }]];
+    
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    if (numbersArray.count == 0) {
+        [result setArray:namesArray];
+    }
+    if (namesArray.count == 0) {
+        [result setArray:numbersArray];
+    }
+    
+    return result;
 }
 
 @end
