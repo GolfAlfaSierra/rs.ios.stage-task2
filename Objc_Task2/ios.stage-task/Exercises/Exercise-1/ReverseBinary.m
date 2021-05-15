000d662a0385871447bcf55641331e59cbd73a36
Artyom Sog <tomas.tomson@Mail.ru>
2021-05-15 14:28:44 +0300

    feat: exercise one complete

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,27 @@
 #import "ReverseBinary.h"
 
+UInt8 intToBinary(UInt8 num) {
+    uint binaryNum[8] = {0}; // fill byte with zero
+    uint counter = 8 -1; // shift amount
+    
+    uint i = 0; // cursor to array item
+    uint result = 0;
+    
+    while (num > 0) {
+        
+        // storing remainder in binary array
+        binaryNum[i] = num % 2;
+        num = num / 2;
+        
+        result += binaryNum[i] << counter; // counter = 8
+        
+        --counter;
+        ++i;
+    }
+    
+    return result;
+}
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    
+    return intToBinary(n);
 }
