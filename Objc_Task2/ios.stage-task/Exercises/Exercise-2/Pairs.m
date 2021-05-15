7a7a3e91a615a91fd7ca1f5d81ec5dc34903713f
Artyom Sog <tomas.tomson@Mail.ru>
2021-05-15 15:36:31 +0300

    feat exercise two done

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -1,9 +1,34 @@
 #import "Pairs.h"
 
+
+int findPair(NSArray<NSNumber*> *array, NSNumber* target) {
+    int counter = 0;
+    NSMutableSet<NSNumber*>* set = [[NSMutableSet alloc] init];
+    
+    for (NSNumber *num in array){
+        
+        if ([set containsObject:[NSNumber numberWithInt:(int)([num intValue] - [target intValue])]]) {
+            counter+=1;
+        }
+        
+        if ([set containsObject:[NSNumber numberWithInt:(int)([num intValue] + [target intValue])]]) {
+            counter+=1;
+        }
+        
+        [set addObject:num];
+    }
+    
+    return counter;
+}
+
 @implementation Pairs
 
+
+
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    NSInteger result = findPair(array, number);
+    
+    return result;
 }
 
 @end
